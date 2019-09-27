f72745bbf5decc9b2e027d5b934bf89b986a8f4d
tu.le <tu.le@persol.co.jp>
2019-09-27 09:58:32 +0700

    fix config sitespeed.io

--- run-sitespeed.sh
+++ run-sitespeed.sh
@@ -5,8 +5,8 @@ config=$3
 if [ -z "$args" ]
   then
     echo "Start execute sitespeed test for ${url} with no agruments!"
-    docker-compose run --rm sitespeed.io --config=./${config:-"config.json"} ${url}
+    docker-compose run --rm sitespeed.io --config ${config} --useHash ${url}
   else
     echo "Start execute sitespeed test for ${url} with agruments: ${args}"
-    docker-compose run --rm sitespeed.io --config=./${config:-"config.json"} ${url} ${args}
+    docker-compose run --rm sitespeed.io --config ${config} --useHash ${url} --preScript ${args}
 fi
--- sitespeed/pre.js
+++ sitespeed/pre.js
@@ -1,27 +1,25 @@
-// https://beenanner.github.io/sitespeed.io/documentation/sitespeed.io/prepostscript/
+// https://www.sitespeed.io/documentation/sitespeed.io/scripting/#run
 
-module.exports = {
-  run(context) {
-    return context.runWithDriver((driver) => {
-      // Go to login URL
-      return driver.get('https://kite-dev-it-vn.hito-link.jp/#/login')
-        .then(() => {
+module.exports = async function(context, commands) {
+  await commands.navigate(
+    'https://kite-dev-it-vn.hito-link.jp/#/login'
+  );
 
-          // we fetch the selenium webdriver from context
-          const webdriver = context.webdriver;
-          const By = webdriver.By;
-
-          // before you start, make your tenantCd, email and password
-          const tenantCd = 'ppt';
-          const email = 'tu.le@persol.co.jp';
-          const password = 'Tu@123456';
-          driver.findElement(By.name('tenantCd')).sendKeys(tenantCd);
-          driver.findElement(By.name('email')).sendKeys(email);
-          driver.findElement(By.name('password')).sendKeys(password);
-          const loginButton = driver.findElement(webdriver.By.xpath("//button[contains(.,'Login')]"));
-          loginButton.click();
-          
-        });
-    })
+  try {
+    await commands.addText.byClassName('ppt', 'js-tenant-module input input-middle');
+    await commands.addText.byClassName('tu.le@persol.co.jp', 'js-email-module input input-middle');
+    await commands.addText.byClassName('123456', 'js-password-module input input-middle');
+    // Click on the submit button with id wpLoginAttempt
+    await commands.click.byClassName('btn btn-ghost');
+    // wait on a specific id to appear on the page after you logged in
+    return commands.wait.byTime(10000);
+  } catch (e) {
+    // We try/catch so we will catch if the the input fields can't be found
+    // The error is automatically logged in Browsertime and re-thrown here
+    // We could have an alternative flow ...
+    // else we can just let it cascade since it catched later on and reported in
+    // the HTML
+    // commands.error('Error!!!');
+    throw e;
   }
 };
